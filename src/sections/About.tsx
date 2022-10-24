e9870defb2b5fa27c45bb019e4c9cfb3b75130c4
MarioPayan <mpayan.av@gmail.com>
2022-10-23 21:44:59 -0500

    Fixing shared links

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import {Paper, Typography} from '@mui/material'
+import { Grid, Paper, Typography } from '@mui/material'
 
 const About = () => {
   return (
     <>
       <Paper elevation={5}>
-        <Typography variant='h1'>Hey! I&apos;m Mario Payan</Typography>
-        <Typography variant='h4'>
-          A Fullstack developer with focus on software development, algorithms design, software as a service and clean
-          code. Challenge driven, always willing to learn, passionate about new technologies, teamwork and code writing
-        </Typography>
+        <Grid item container padding={5}>
+          <Grid item xs={8}><Typography variant='h3'>Hey!</Typography></Grid>
+          <Grid item xs={8}><Typography variant='h3'>I&apos;m Mario Payan</Typography></Grid>
+        </Grid>
+      </Paper>
+      <Paper elevation={5}>
+        <Grid item container padding={5}>
+          <Grid item xs={8}><Typography variant='h5'>A Fullstack developer with focus on software development, algorithms design, software as a service and clean
+            code. Challenge driven, always willing to learn, passionate about new technologies, teamwork and code writing
+          </Typography></Grid>
+        </Grid>
       </Paper>
     </>
   )
